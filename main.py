a1ee7ce8e43035b1076287369b053d31999db64a
wooferOS <woofer.bot.ua@gmail.com>
2025-05-28 13:07:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -172,5 +172,7 @@ async def main():
     logging.info("🤖 Бот запущено через polling")
     await app.run_polling()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    import asyncio
     asyncio.run(main())
+
