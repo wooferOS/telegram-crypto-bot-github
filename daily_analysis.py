ea223a236b9e59a4c3e628be00043502382ac443
wooferOS <woofer.bot.ua@gmail.com>
2025-05-30 19:39:33 +0300

    Update daily_analysis.py

--- daily_analysis.py
+++ daily_analysis.py
@@ -177,6 +177,7 @@ def send_telegram_report(text, path=None):
             bot.send_message(chat_id=ADMIN_CHAT_ID, text=text, parse_mode=ParseMode.MARKDOWN)
     except Exception as e:
         logging.error(f"❌ Помилка при надсиланні в Telegram: {e}")
+        
         def get_binance_balances():
     try:
         account_info = client.get_account()
@@ -193,6 +194,7 @@ def send_telegram_report(text, path=None):
     except Exception as e:
         logging.error(f"❌ Не вдалося отримати баланс Binance: {str(e)}")
         return {}
+
 def main():
     try:
         log_message("🔁 Запуск daily_analysis.py")
