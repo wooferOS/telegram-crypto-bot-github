a373afe736cce08406da00eda6d6ec57300f3d84
wooferOS <woofer.bot.ua@gmail.com>
2025-05-30 16:14:10 +0300

    Update restart_bot.sh

--- restart_bot.sh
+++ restart_bot.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export $(cat /root/telegram-crypto-bot-github/.env | xargs)
+
 echo "🔁 Перезапуск Telegram GPT-бота..."
 sudo systemctl restart crypto-bot
 sudo systemctl status crypto-bot --no-pager
