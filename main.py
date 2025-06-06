935261a42efcee88df7ada0c52acf21ee0a9230c
Oleksii Shymanskyi <woofer.bot.ua@gmail.com>
2025-06-06 18:39:05 +0300

    refactor: simplify aiogram usage (#76)

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
-import asyncio
 import logging
-
+from aiogram import Bot, Dispatcher, types, executor
 from telegram_bot import dp, bot, setup_scheduler
 
 
 logging.basicConfig(level=logging.INFO)
 
 
-async def main() -> None:
+async def on_startup(dispatcher: Dispatcher) -> None:
     setup_scheduler()
     await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    executor.start_polling(dp, on_startup=on_startup)
