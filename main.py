92dc2a12078511bb0e99fdf2276bf0fcbde65c8d
Oleksii Shymanskyi <woofer.bot.ua@gmail.com>
2025-06-07 09:02:40 +0300

    Add explicit handler registration (#106)

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
-from telegram_bot import dp, bot, setup_scheduler
+from telegram_bot import dp, bot, setup_scheduler, register_handlers
 
 
 logging.basicConfig(level=logging.INFO)
 
 
 async def on_startup(dispatcher: Dispatcher) -> None:
-    setup_scheduler()
     await bot.delete_webhook(drop_pending_updates=True)
 
 
 if __name__ == "__main__":
+    setup_scheduler()
+    register_handlers(dp)
     executor.start_polling(dp, on_startup=on_startup)
