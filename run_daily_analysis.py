8781e8efc0bc56887f98cc597c95215b11764996
Oleksii Shymanskyi <woofer.bot.ua@gmail.com>
2025-06-13 11:43:15 +0300

    Fix daily analysis launch (#283)

--- run_daily_analysis.py
+++ run_daily_analysis.py
@@ -1,13 +1,7 @@
 import asyncio
-import sys
 
-from daily_analysis import daily_analysis_task, send_zarobyty_forecast
-from telegram_bot import bot, ADMIN_CHAT_ID
+from daily_analysis import daily_analysis_task
 
 
 if __name__ == "__main__":
-    task = sys.argv[1] if len(sys.argv) > 1 else "report"
-    if task == "forecast":
-        asyncio.run(send_zarobyty_forecast(bot, ADMIN_CHAT_ID))
-    else:
-        asyncio.run(daily_analysis_task(bot, ADMIN_CHAT_ID))
+    asyncio.run(daily_analysis_task())
