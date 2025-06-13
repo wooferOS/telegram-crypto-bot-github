f5f7d218a420a733921aa9db38de994bcb64b476
Oleksii Shymanskyi <woofer.bot.ua@gmail.com>
2025-06-13 11:52:05 +0300

    fix: pass bot and chat id to daily analysis (#284)

--- run_daily_analysis.py
+++ run_daily_analysis.py
@@ -1,7 +1,8 @@
 import asyncio
-
+from telegram_bot import bot
+from config import CHAT_ID
 from daily_analysis import daily_analysis_task
 
 
 if __name__ == "__main__":
-    asyncio.run(daily_analysis_task())
+    asyncio.run(daily_analysis_task(bot, CHAT_ID))
