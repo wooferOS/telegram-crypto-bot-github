fd838c1fbc8ff61abe48fca9fda3ce0f13c19bea
Oleksii Shymanskyi <woofer.bot.ua@gmail.com>
2025-06-13 23:23:19 +0300

    Update start_trade_cycle.sh (#299)

--- start_trade_cycle.sh
+++ start_trade_cycle.sh
@@ -1,15 +1,9 @@
-echo '#!/bin/bash
-cd /root/telegram-crypto-bot-github
-export $(grep -v "^#" systemd/crypto-bot.env | xargs)
-python3 run_auto_trade_cycle.py >> /root/trade.log 2>&1' > /root/start_trade_cycle.sh && \
-chmod +x /root/start_trade_cycle.sh && \
-(crontab -l 2>/dev/null; echo "15 * * * * /root/start_trade_cycle.sh") | crontab -
-
-
 cat << 'EOF' > /root/start_trade_cycle.sh
 #!/bin/bash
 cd /root/telegram-crypto-bot-github
-export $(grep -v '^#' systemd/crypto-bot.env | xargs)
+set -a
+. systemd/crypto-bot.env
+set +a
 exec /usr/bin/python3 run_auto_trade_cycle.py >> /root/trade.log 2>&1
 EOF
 
