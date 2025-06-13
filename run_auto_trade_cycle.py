95292630ed371d6074b153b5b13111007037961c
Oleksii Shymanskyi <woofer.bot.ua@gmail.com>
2025-06-13 13:30:09 +0300

    Log to file and console

--- run_auto_trade_cycle.py
+++ run_auto_trade_cycle.py
@@ -1,12 +1,19 @@
 import asyncio
 import logging
+import sys
 
 from telegram_bot import bot
 from config import CHAT_ID
 from auto_trade_cycle import auto_trade_cycle
 
 LOG_PATH = "/root/trade.log"
-logging.basicConfig(filename=LOG_PATH, level=logging.INFO)
+logging.basicConfig(
+    level=logging.INFO,
+    handlers=[
+        logging.FileHandler(LOG_PATH),
+        logging.StreamHandler(sys.stdout),
+    ],
+)
 
 if __name__ == "__main__":
     chat_id = int(CHAT_ID) if CHAT_ID else 0
