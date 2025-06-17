2bad4e9d3cb31dbe50da72f414cbcafa7dc98aaa
Oleksii Shymanskyi <woofer.bot.ua@gmail.com>
2025-06-17 12:17:58 +0300

    Relax profit thresholds to always generate signals

--- config.py
+++ config.py
@@ -3,7 +3,9 @@ import os
 # Thresholds for trading decisions
 # Updated trading thresholds
 MIN_PROB_UP = 0.45
-MIN_EXPECTED_PROFIT = 0.3
+# Minimum expected profit to consider a token attractive.
+# Reduced to allow generation of signals even with tiny profit
+MIN_EXPECTED_PROFIT = 0.01
 MIN_TRADE_AMOUNT = float(os.getenv("MIN_TRADE_AMOUNT", 10))  # USDT
 
 # Auto trading loop interval in seconds
