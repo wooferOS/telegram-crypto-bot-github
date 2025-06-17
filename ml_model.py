d1407ae8ab01ae981407d06f46bca200e2a1376b
Oleksii Shymanskyi <woofer.bot.ua@gmail.com>
2025-06-17 15:44:26 +0300

    refactor: remove getenv usage (#338)

--- ml_model.py
+++ ml_model.py
@@ -12,7 +12,9 @@ from binance_api import _to_usdt_pair, is_symbol_valid
 
 MODEL_PATH = "model.joblib"
 
-client = Client(api_key=os.getenv("BINANCE_API_KEY"), api_secret=os.getenv("BINANCE_API_SECRET"))
+from config import BINANCE_API_KEY, BINANCE_SECRET_KEY
+
+client = Client(api_key=BINANCE_API_KEY, api_secret=BINANCE_SECRET_KEY)
 
 def load_model():
     if os.path.exists(MODEL_PATH):
