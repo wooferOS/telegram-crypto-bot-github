6cf1e3f04372b456781a9368334f376d1e1eb30e
Oleksii Shymanskyi <woofer.bot.ua@gmail.com>
2025-06-18 14:31:10 +0300

    Log forecast messages to trade.log (#366)

--- run_daily_analysis.py
+++ run_daily_analysis.py
@@ -1,11 +1,14 @@
 import asyncio
 import sys
 
+from log_setup import setup_logging
+
 from daily_analysis import daily_analysis_task, send_zarobyty_forecast
 from telegram_bot import bot, ADMIN_CHAT_ID
 
 
 if __name__ == "__main__":
+    setup_logging()
     task = sys.argv[1] if len(sys.argv) > 1 else "report"
     if task == "forecast":
         asyncio.run(send_zarobyty_forecast(bot, ADMIN_CHAT_ID))
