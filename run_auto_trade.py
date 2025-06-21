f32dfd91ee03f3e907310feb1d73d6b24225f7f5
Oleksii Shymanskyi <woofer.bot.ua@gmail.com>
2025-06-21 13:41:21 +0300

    Fix tuple unpacking for generate_conversion_signals (#429)

--- run_auto_trade.py
+++ run_auto_trade.py
@@ -116,6 +116,8 @@ if __name__ == "__main__":
         _,
         _,
         _,
+        _,
+        _,
         gpt_forecast,
         predictions,
     ) = generate_conversion_signals(gpt_filters, gpt_forecast)
